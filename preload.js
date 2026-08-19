98ebc4e5e70605ee2da47292db5f93b7400bab34
Www.Fedpromptly.com <thelytenp5@gmail.com>
2026-08-19 17:35:27 -0400

    Clear electronAPI methods in preload.js
    Removed all exposed API methods from electronAPI.

--- preload.js
+++ preload.js
@@ -1,13 +1,2 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openWindow: (incognito) => ipcRenderer.invoke('open-window', incognito),
-  getVersion: () => ipcRenderer.invoke('get-version'),
-  openExternal: (url) => ipcRenderer.invoke('open-external', url),
-  showSaveDialog: (options) => ipcRenderer.invoke('show-save-dialog', options),
-  writeFile: (path, data) => ipcRenderer.invoke('write-file', path, data),
-  readFile: (path) => ipcRenderer.invoke('read-file', path),
-  clearBrowsingData: (options) => ipcRenderer.invoke('clear-browsing-data', options),
-  getDownloadPath: () => ipcRenderer.invoke('get-download-path'),
-  setDefaultBrowser: () => ipcRenderer.invoke('set-default-browser'),
-});
+const { contextBridge } = require('electron');
+contextBridge.exposeInMainWorld('electronAPI', {});
