8a5404ef62240efd4aa95ec174cb571764ebb2d5
Www.Fedpromptly.com <thelytenp5@gmail.com>
2026-08-19 16:13:11 -0400

    Expose additional Electron APIs in preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('browserAPI', {
-  newWindow: (url) => ipcRenderer.send('new-window-request', url)
+contextBridge.exposeInMainWorld('electronAPI', {
+  openWindow: (incognito) => ipcRenderer.invoke('open-window', incognito),
+  getVersion: () => ipcRenderer.invoke('get-version'),
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
+  showSaveDialog: (options) => ipcRenderer.invoke('show-save-dialog', options),
+  writeFile: (path, data) => ipcRenderer.invoke('write-file', path, data),
+  readFile: (path) => ipcRenderer.invoke('read-file', path),
+  clearBrowsingData: (options) => ipcRenderer.invoke('clear-browsing-data', options),
+  getDownloadPath: () => ipcRenderer.invoke('get-download-path'),
+  setDefaultBrowser: () => ipcRenderer.invoke('set-default-browser'),
 });
