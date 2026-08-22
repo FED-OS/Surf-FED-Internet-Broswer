1d4905de0eacb2d11c75db5f843752f4550b4c5c
Www.Fedpromptly.com <thelytenp5@gmail.com>
2026-08-22 09:04:24 -0400

    Add Electron API for window minimize, maximize, close
    Expose Electron API methods for window control.

--- preload.js
+++ preload.js
@@ -1,1 +1,7 @@
-// Intentionally empty – no Node APIs exposed to web content.
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  minimize: () => ipcRenderer.send('window:minimize'),
+  maximize: () => ipcRenderer.send('window:maximize'),
+  close: () => ipcRenderer.send('window:close'),
+});
